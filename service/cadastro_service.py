2e6f41431ce5234abada34bd632c3d68e5bb7a52
Augusto Pasquali <augusto.pasqualidelima@gmail.com>
2025-06-28 17:04:13 -0300

    Adicionando arquivos

--- service/cadastro_service.py
+++ service/cadastro_service.py
@@ -1,20 +1,19 @@
 from model.professor import Professor
 from repository.professor_repository import ProfessorRepository
-from typing import List, Optional
 
 
 class CadastroService:
-    def __init__(self) -> None:
+    def __init__(self):
         self.professor_repository = ProfessorRepository()
 
-    def cadastrar_professor(self, nome: str, email: str) -> Professor:
+    def cadastrar_professor(self, nome: str, email: str):
         novo_id: int = len(self.professor_repository.listar_todos()) + 1
         professor = Professor(id=novo_id, nome=nome, email=email)
         self.professor_repository.salvar(professor)
         return professor
 
-    def listar_professores(self) -> List[Professor]:
+    def listar_professores(self):
         return self.professor_repository.listar_todos()
 
-    def buscar_professor_por_id(self, id: int) -> Optional[Professor]:
+    def buscar_professor_por_id(self, id: int):
         return self.professor_repository.buscar_por_id(id)
